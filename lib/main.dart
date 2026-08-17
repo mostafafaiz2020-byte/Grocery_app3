9d119a4307a27bde118c11eaf949419c36a453f5
Mostafa faiz <mostafa.faiz2020@gmail.com>
2026-08-17 21:45:36 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,3 +6,6 @@ import 'package:grocery_app1/grocery_app.dart';
 void main() {
   runApp(const GroceryApp());
 }
+/*I added an extra screen at the beginning with a button. When I press the button, it opens the Login page. After logging in and pressing the Login button, it opens the Shop page.
+
+I finished the required work on the Shop page and started working on something new, but I haven’t finished it yet.
